9b01065d86109f3924f14379ff4284ec9e3d0862
joannamhead <joannamhead@hotmail.co.uk>
2024-03-01 18:57:08 +0000

    text colour change on navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -10,25 +10,14 @@ function NavBar() {
     
     <Navbar expand="lg" className="bg-body-tertiary bg-danger">
       <Container fluid>
-        <Navbar.Brand href="#home">Slice and Sizzle</Navbar.Brand>
+        <Navbar.Brand href="#home" className="text-light">Slice and Sizzle</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#aboutUs">About Us</Nav.Link>
-            <Nav.Link href="#menu">Menu</Nav.Link>
-            <Nav.Link href="#yourOrder">Your Order</Nav.Link>
-            <Nav.Link href="#reviews">Customer Reviews</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#aboutUs" className="text-light">About Us</Nav.Link>
+            <Nav.Link href="#menu" className="text-light">Menu</Nav.Link>
+            <Nav.Link href="#yourOrder" className="text-light">Your Order</Nav.Link>
+            <Nav.Link href="#reviews" className="text-light">Customer Reviews</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
