b0d4d19a38e9fa209a9fb60cc52ee3147cee9c76
fumitsukai <pandeleaalexandru321@gmail.com>
2024-03-02 14:46:23 +0000

    added login endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,30 @@ import bcrypt from "bcrypt";
 
 //authenticate user
 //route POST /api/users/login
-const Login = (req, res) => {
+const Login = async (req, res) => {
   const { email, password } = req.body;
+  //check for email
+  if (!email) {
+    res.status(403);
+    throw new Error("Please enter your email");
+  }
+  //check for password
+  if (!password) {
+    res.status(403);
+    throw new Error("Please enter your password");
+  }
+  //check db for email
+  const { data, error } = await db.from("User").select("*").eq("email", email);
 
+  //compare password with db
+
+  const pass = await bcrypt(password, data[0].password);
+
+  //check username exists and password is correct
+  if (data.length != 1 || !pass) {
+    res.status(403);
+    throw new Error("Invalid username or password");
+  }
   res.status(200).json({ message: "User logged in!" });
 };
 
