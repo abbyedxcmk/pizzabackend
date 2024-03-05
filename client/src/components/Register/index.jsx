8d65d46df211e6716ecd9b5f27e18c875c677407
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-03-05 20:34:04 +0000

    Added playwright yaml file

--- client/src/components/Register/index.jsx
+++ client/src/components/Register/index.jsx
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import NavBar from "../NavBar";
+import NavBar from "../Navbar";
 import { useState } from "react";
 import axios from "axios";
 import "../../index.css";
