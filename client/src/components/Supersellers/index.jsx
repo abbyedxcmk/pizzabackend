3ba4f758c4b2f8341bb73c836ebfbc6b7d5c8bd2
joannamhead <joannamhead@hotmail.co.uk>
2024-03-06 21:14:09 +0000

    added more styling to supersellers

--- client/src/components/Supersellers/index.jsx
+++ client/src/components/Supersellers/index.jsx
@@ -1,53 +1,16 @@
-// // SuperSellers.js
-
-// import React from 'react';
-// import Card from "react-bootstrap/Card";
-// import './index.css'
-// import superseller1 from "../../assets/pizza-images/01.png";
-// import superseller2 from
-// import superseller3 from
-
-// function SuperSellers() {
-//   const superSellersData = [
-//     { id: 1, name: 'Super Seller 1', image: superseller1 },
-//     { id: 2, name: 'Super Seller 2', image: superseller2 },
-//     { id: 3, name: 'Super Seller 3', image: superseller3 },
-    
-//   ];
-
-//   return (
-// <div id="Supersellers">
-//     <div className="super-sellers">
-//       <h2>Super Sellers</h2>
-//       <div>
-//         {superSellersData.map((seller) => (
-//           <Card key={seller.id}>
-//             <Card.Img variant="top" src={seller.image} alt={seller.name} />
-//             <Card.Body>
-//               <Card.Title>{seller.name}</Card.Title>
-//             </Card.Body>
-//           </Card>
-//         ))}
-//       </div>
-//     </div>
-//     </div>
-
-//   );
-// };
-
-// export default SuperSellers;
-
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import superseller1 from "../../assets/pizza-images/01.png";
 import superseller2 from "../../assets/pizza-images/02.png";
 import superseller3 from "../../assets/pizza-images/03.png";
+import './index.css';
 
 function GroupExample() {
   return (
-    <CardGroup>
-      <Card>
+    
+    <CardGroup id="cards">
+      <Card id="card1">
         <Card.Img variant="top" src={superseller1} />
         <Card.Body>
           <Card.Title>Card title</Card.Title>
@@ -56,9 +19,9 @@ function GroupExample() {
             to additional content. This content is a little bit longer.
           </Card.Text>
         </Card.Body>
-        
       </Card>
-      <Card>
+      
+      <Card id="card2">
         <Card.Img variant="top" src={superseller2} />
         <Card.Body>
           <Card.Title>Card title</Card.Title>
@@ -67,9 +30,9 @@ function GroupExample() {
             additional content.{' '}
           </Card.Text>
         </Card.Body>
-        
       </Card>
-      <Card>
+
+      <Card id="card3">
         <Card.Img variant="top" src={superseller3} />
         <Card.Body>
           <Card.Title>Card title</Card.Title>
