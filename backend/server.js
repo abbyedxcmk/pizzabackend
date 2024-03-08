9bf6392d38567bb41ee9057ec410fa2a7b6652e6
fumitsukai <pandeleaalexandru321@gmail.com>
2024-03-08 17:06:24 +0000

    changed server middleware

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,18 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import pizzaRoutes from "./routes/pizzaRoutes.js";
 import cors from "cors";
-
 dotenv.config();
-
-const port = process.env.PORT;
-
+const port = process.env.PORT || 3000; // Default to port 3000 if PORT isn't set
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+// API routes
 app.use("/api/users", userRoutes);
-app.use("/api", pizzaRoutes);
+app.use("/api/pizzas", pizzaRoutes);
+// Catch-all for testing that the server is running
+app.all("/", (req, res) => res.send("Server is ready!"));
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
-app.use(cors());
-
-app.all("/", (req, res) => res.send("Server is ready!"));
-
+// Start the server
 app.listen(port, () => console.log(`Server started at port ${port}`));
